95d1790f726e2dc3c4dd427b89793733e8dcfba9
MinhTran <minhtran0918@gmail.com>
2018-05-31 13:40:11 +0700

    1h40p 31/05

--- app/src/main/java/calebzone/hcmute/edu/vn/happycooking/HomeActivity.java
+++ app/src/main/java/calebzone/hcmute/edu/vn/happycooking/HomeActivity.java
@@ -2,7 +2,10 @@ package calebzone.hcmute.edu.vn.happycooking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*
+    Happy Cooking App
+    Create 31/05/2018
+ */
 public class HomeActivity extends AppCompatActivity {
 
     @Override
