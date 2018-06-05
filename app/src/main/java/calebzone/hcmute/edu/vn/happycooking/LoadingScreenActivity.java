758a3b4446c2984a8db8e0cf99dc963ed55d1368
MinhTran <minhtran0918@gmail.com>
2018-06-05 16:47:46 +0700

    05/06

---
+++ app/src/main/java/calebzone/hcmute/edu/vn/happycooking/LoadingScreenActivity.java
@@ -0,0 +1,13 @@
+package calebzone.hcmute.edu.vn.happycooking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+    }
+}
